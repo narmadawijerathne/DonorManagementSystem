2064d9c2c49bb94b3d89222927fbac6e1984030b
Narmada Wijerathne <narmada@christomartinez.com>
2017-07-26 09:07:58 +0530

    Add project files.

---
+++ DonorManagementSystem/Models/Donations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DonorManagementSystem.Models
+{
+    public class Donations
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Province { get; set; }
+        public string NameonCard { get; set; }
+        public int CardNumber { get; set; }
+        public int CVV { get; set; }        
+        public DateTime PaymentDate { get; set; }
+        public double AmountPaid { get; set; }
+
+    }
+}
---
+++ DonorManagementSystem/Scripts/MyScript.js
@@ -0,0 +1,117 @@
+﻿    function showPanel1() {
+        $("#panel1").show();
+        $("#panel2").hide();
+        $("#panel3").hide();
+        $("#panel4").hide();
+        $("#panel5").hide();
+        $("#defaultPanel").hide();
+
+    }
+
+    function showPanel2() {
+
+        $("#panel1").hide();
+        $("#panel2").show();
+        $("#panel3").hide();
+        $("#panel4").hide();
+        $("#panel5").hide();
+        $("#defaultPanel").hide();
+
+    }
+
+    function showPanel3() {
+        $("#panel1").hide();
+        $("#panel2").hide();
+        $("#panel3").show();
+        $("#panel4").hide();
+        $("#panel5").hide();
+        $("#defaultPanel").hide();
+    }
+
+    function showPanel4() {
+        $("#panel1").hide();
+        $("#panel2").hide();
+        $("#panel3").hide();
+        $("#panel4").show();
+        $("#panel5").hide();
+        $("#defaultPanel").hide();
+    }
+
+    function showPanel5() {
+        $("#panel1").hide();
+        $("#panel2").hide();
+        $("#panel3").hide();
+        $("#panel4").hide();
+        $("#panel5").show();
+        $("#defaultPanel").hide();
+    }
+
+    function showTextBox() {
+        $("#btT").show();
+        $("#btText").focus();
+        $("#mybutton").hide();
+
+    }
+
+    function showModal() {
+        $("#myModel").modal.hide();
+        $("#contentModel").modal.show();
+    }
+
+    //show button amount on textbox within modal
+
+    $(document).on("click", "#bt1", function () {
+        var myBookId = $("#bt1").data('id');
+        $(".modal-body #donationAmount").text(myBookId);
+    });
+
+
+    //display textbox
+    function displayname() {
+
+        var result = document.getElementById("btText").value;
+        $(".modal-body #donationAmount").val(result);
+    }
+
+    var btValue = [];
+
+    function myFunction1() {
+        btValue.push("15");
+    }
+    function myFunction2() {
+        btValue.push("35");
+    }
+    function myFunction3() {
+        btValue.push("65");
+    }
+    function myFunction4() {
+        btValue.push("100");
+    }
+    function myFunction5() {
+        btValue.push("250");
+    }
+
+
+
+    function submitData() {
+        console.log(btValue.length);
+        if (btValue.length == 0)
+        {
+            //$('#myModal').modal('hide');
+            $('#myModal').appendTo("body").modal('hide');
+            $("#errorMsg").show();
+        }
+
+        else {
+            $('#myModal').modal('show');
+        }
+    }
+
+$(document).ready(function () {
+    $("#btT").focusout(function () {
+        $("#btText").val('');
+        $("#mybutton").show();
+        $("#btT").hide(); 
+    });
+    });
+
